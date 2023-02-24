b104734846a0dc8e1bfdc77a391af37b23bcd05b
Zodo <malaizyogmuzumala@gmail.com>
2023-02-24 13:07:36 +0200

    more stuff done

--- build/js/main.js
+++ build/js/main.js
@@ -1,1 +1,15 @@
 "use strict";
+//literal types
+let myName;
+let userName;
+userName = "Bob";
+//function types
+const add = (a, b) => {
+    return a + b;
+};
+const logMsg = (message) => {
+    console.log(message);
+};
+logMsg("Hello");
+logMsg(add(2, 3));
+logMsg(add('a ', 3));
--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,26 @@ type Guitarist = {
   active: boolean,
   albums: stringOrNumberArray
 }
+
+type UserID = StringOrNumber;
+
+//literal types
+
+let myName: 'Dave'
+
+let userName: 'Dave' | 'Bob' | 'Alice';
+userName = "Bob";
+
+//function types
+
+const add = (a: number, b:number ): number =>{
+  return a + b;
+}
+
+const logMsg = (message:any): void =>{
+  console.log(message);
+}
+
+logMsg("Hello");
+logMsg(add(2,3));
+logMsg(add('a ',3));
