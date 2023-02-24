8f57a8c339d440ab8afbfc42b15177ce4c151dea
Zodo <malaizyogmuzumala@gmail.com>
2023-02-24 18:17:23 +0200

    functions

--- build/js/main.js
+++ build/js/main.js
@@ -16,7 +16,27 @@ logMsg(add(7, 3));
 let subtract = function (c, d) {
     return c - d;
 };
+// interface MathFunc {(a: number, b: number):number;}
 let multiply = function (c, d) {
     return c * d;
 };
 console.log(multiply(2, 3));
+//optional parameters
+const addAll = (a, b, c) => {
+    if (typeof c !== 'undefined') {
+        return a + b + c;
+    }
+    return a + b;
+};
+//default parameters
+const sumAll = (a = 2, b, c = 2) => {
+    return a + b + c;
+};
+logMsg(addAll(2, 3, 2));
+logMsg(addAll(2, 3));
+logMsg(sumAll(2, 3));
+//rest parameters
+const total = (...nums) => {
+    return nums.reduce((prev, curr) => prev + curr);
+};
+logMsg(total(1, 2, 3, 4));
--- src/main.ts
+++ src/main.ts
@@ -36,11 +36,34 @@ let subtract = function (c: number, d: number): number{
   return c - d;
 }
 
-// type MathFunc = (a: number, b: number) => number;
-interface MathFunc {(a: number, b: number):number;}
+type MathFunc = (a: number, b: number) => number;
+// interface MathFunc {(a: number, b: number):number;}
 
 let multiply: MathFunc = function (c,d){
   return c * d;
 }
 
 console.log(multiply(2,3));
+
+//optional parameters
+const addAll = (a:number, b:number, c?:number): number =>{
+  if(typeof c !== 'undefined'){
+return a+b+c;
+}
+return a+b;
+}
+//default parameters
+const sumAll = (a:number = 2, b:number, c:number=2): number =>{
+return a+b+c;
+}
+
+logMsg(addAll(2,3,2));
+logMsg(addAll(2,3,));
+logMsg(sumAll(2,3));
+
+//rest parameters
+const total = (...nums:number[]):number =>{
+  return nums.reduce((prev, curr) => prev + curr)
+}
+
+logMsg(total(1,2,3,4));
