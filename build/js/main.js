29f7af84181c5de7ab08b23762f03c1c161983e4
Zodo <malaizyogmuzumala@gmail.com>
2023-02-24 11:28:12 +0200

    goingtupilwe rose ponya

--- build/js/main.js
+++ build/js/main.js
@@ -5,3 +5,12 @@ let myTuple = ['a', 1];
 let myObj;
 myObj = [];
 console.log(typeof myObj);
+const exampleObject = {
+    prop1: 'malaizyo',
+    prop2: true,
+};
+let evh = {
+    name: 'Eddie Van Halen',
+    active: true,
+    albums: [1984, '5150', 'For Unlawful Carnal Knowledge']
+};
--- src/main.ts
+++ src/main.ts
@@ -7,3 +7,20 @@ let myTuple: [string, number] = ['a', 1];
 let myObj: object
 myObj = []
 console.log(typeof myObj)
+
+const exampleObject = {
+  prop1: 'malaizyo',
+  prop2: true,
+}
+
+type Guitarist = {
+  name: string,
+  active: boolean,
+  albums: (string | number)[]
+}
+
+let evh: Guitarist = {
+  name: 'Eddie Van Halen',
+  active: true,
+  albums:[1984, '5150', 'For Unlawful Carnal Knowledge']
+}
