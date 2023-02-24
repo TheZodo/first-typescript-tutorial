89bab8ab375ef04779914c7e62d6d16d07dc3879
Zodo <malaizyogmuzumala@gmail.com>
2023-02-24 11:45:43 +0200

    Beginning of episode 4

--- src/main.ts
+++ src/main.ts
@@ -1,54 +1,10 @@
-let stringArr = ['a', 'b', 'c'];
+//type aliases
+type StringOrNumber = string | number;
 
-stringArr.unshift('d');
+type stringOrNumberArray = (string | number)[];
 
-let myTuple: [string, number] = ['a', 1];
-
-let myObj: object
-myObj = []
-console.log(typeof myObj)
-
-const exampleObject = {
-  prop1: 'malaizyo',
-  prop2: true,
-}
-
-interface Guitarist {
+type Guitarist = {
   name?: string,
   active: boolean,
-  albums: (string | number)[]
+  albums: stringOrNumberArray
 }
-
-let evh: Guitarist = {
-  name: 'Eddie Van Halen',
-  active: true,
-  albums:[1984, '5150', 'For Unlawful Carnal Knowledge']
-}
-
-let JP: Guitarist = {
-  name: 'Joe Perry',
-  active: false,
-  albums: ['Rockin\' the Joint', 'Rocks', 'Let the Music Do the Talking']
-}
-
-evh = JP
-
-const greetGuitarist = (guitarist: Guitarist) => {
-  if(guitarist.name){
-    return `Hello ${guitarist.name.toUpperCase()}`
-  }
-  return 'Hello'
-}
-
-console.log(greetGuitarist(evh))
-
-// Enums
-enum Grade{
-  U = 1,
-  D,
-  C,
-  B,
-  A
-}
-
-console.log(Grade.U)
