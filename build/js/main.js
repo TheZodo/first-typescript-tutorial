889b35f46aec0c854b43f6620c3d5f1489337a03
Zodo <malaizyogmuzumala@gmail.com>
2023-02-25 10:18:12 +0200

    more work

--- build/js/main.js
+++ build/js/main.js
@@ -1,19 +1,20 @@
 "use strict";
-let a = "hello";
+let a = 'hello';
 let b = a; // assignment to a less specific type
 let c = a; // assignment to a more specific type
-let d = "World";
-let e = "World";
+let d = 'World';
+let e = 'World';
 const addOrConcat = (a, b, c) => {
-    if (c === "add") {
+    if (c === 'add') {
         return a + b;
     }
     else {
         return `${a}${b}`;
     }
 };
-let myVal = addOrConcat(2, 2, "concat");
+let myVal = addOrConcat(2, 2, 'concat');
 //be careful
-let nextVal = addOrConcat(2, 2, "concat");
+let nextVal = addOrConcat(2, 2, 'concat');
 //The Dom
-const img = document.querySelector("img");
+const img = document.querySelector('img');
+const myImg = document.getElementById('#img');
--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,31 @@
-type One = string;
-type Two = string | number;
-type Three = "hello";
+type One = string
+type Two = string | number
+type Three = 'hello'
 
-let a: One = "hello";
-let b = a as Two; // assignment to a less specific type
-let c = a as Three; // assignment to a more specific type
+let a: One = 'hello'
+let b = a as Two // assignment to a less specific type
+let c = a as Three // assignment to a more specific type
 
-let d = <One>"World";
-let e = <string | number>"World";
+let d = <One>'World'
+let e = <string | number>'World'
 
 const addOrConcat = (
   a: number,
   b: number,
-  c: "add" | "concat"
+  c: 'add' | 'concat'
 ): number | string => {
-  if (c === "add") {
-    return a + b;
+  if (c === 'add') {
+    return a + b
   } else {
-    return `${a}${b}`;
+    return `${a}${b}`
   }
-};
+}
 
-let myVal: string = addOrConcat(2, 2, "concat") as string;
+let myVal: string = addOrConcat(2, 2, 'concat') as string
 
 //be careful
-let nextVal: number = addOrConcat(2, 2, "concat") as number;
+let nextVal: number = addOrConcat(2, 2, 'concat') as number
 
 //The Dom
-const img = document.querySelector("img") as HTMLImageElement;
+const img = document.querySelector('img')
+const myImg = document.getElementById('#img')
