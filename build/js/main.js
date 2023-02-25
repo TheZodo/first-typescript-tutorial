9dd8bcff2196bd8bb9d7856a1f3acf7be525825a
Zodo <malaizyogmuzumala@gmail.com>
2023-02-25 10:10:13 +0200

    more

--- build/js/main.js
+++ build/js/main.js
@@ -2,3 +2,16 @@
 let a = "hello";
 let b = a; // assignment to a less specific type
 let c = a; // assignment to a more specific type
+let d = "World";
+let e = "World";
+const addOrConcat = (a, b, c) => {
+    if (c === "add") {
+        return a + b;
+    }
+    else {
+        return `${a}${b}`;
+    }
+};
+let myVal = addOrConcat(2, 2, "concat");
+//be careful
+let nextVal = addOrConcat(2, 2, "concat");
--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,23 @@ type Three = "hello";
 let a: One = "hello";
 let b = a as Two; // assignment to a less specific type
 let c = a as Three; // assignment to a more specific type
+
+let d = <One>"World";
+let e = <string | number>"World";
+
+const addOrConcat = (
+  a: number,
+  b: number,
+  c: "add" | "concat"
+): number | string => {
+  if (c === "add") {
+    return a + b;
+  } else {
+    return `${a}${b}`;
+  }
+};
+
+let myVal: string = addOrConcat(2, 2, "concat") as string;
+
+//be careful
+let nextVal: number = addOrConcat(2, 2, "concat") as number;
