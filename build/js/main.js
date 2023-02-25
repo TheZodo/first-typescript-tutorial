e2ab892797706062975f4f5d08ec942b984aede4
Zodo <malaizyogmuzumala@gmail.com>
2023-02-25 09:58:56 +0200

    type assertion

--- build/js/main.js
+++ build/js/main.js
@@ -1,3 +1,4 @@
 "use strict";
 let a = "hello";
 let b = a; // assignment to a less specific type
+let c = a; // assignment to a more specific type
--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,4 @@ type Three = "hello";
 
 let a: One = "hello";
 let b = a as Two; // assignment to a less specific type
+let c = a as Three; // assignment to a more specific type
