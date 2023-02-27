265c9259ec84fcf6bc3276d412001e3a6b49f33e
Zodo <malaizyogmuzumala@gmail.com>
2023-02-27 07:27:16 +0200

    more

--- build/js/main.js
+++ build/js/main.js
@@ -10,5 +10,46 @@ class Coder {
         this.age = age;
         this.lang = lang;
     }
+    getAge() {
+        return this.age;
+    }
 }
 const Malaizyo = new Coder('Malaizyo', 'Rap', 20, 'JavaScript');
+console.log(Malaizyo.getAge());
+// console.log(Malaizyo.lang)
+// console.log(Malaizyo.age)
+class WebDev extends Coder {
+    constructor(computer, name, music, age) {
+        super(name, music, age);
+        this.computer = computer;
+        this.name = name;
+        this.computer = computer;
+    }
+    getLang() {
+        return `I'm a ${this.lang} developer`;
+    }
+}
+const Sara = new WebDev('MacBook', 'Sara', 'Pop', 20);
+console.log(Sara.getLang());
+class Peeps {
+    static getCount() {
+        return Peeps.count;
+    }
+    constructor(name) {
+        this.name = name;
+        this.id = ++Peeps.count;
+    }
+}
+Peeps.count = 0;
+const Zodo = new Peeps('Zodo');
+const Zozo = new Peeps('Zozo');
+const Koko = new Peeps('Koko');
+console.log(Peeps.getCount());
+class Bands {
+    constructor() {
+        this.dataState = [];
+    }
+    get data() {
+        return this.dataState;
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,65 @@ class Coder {
     this.age = age
     this.lang = lang
   }
+
+  public getAge(): number {
+    return this.age
+  }
 }
 
 const Malaizyo = new Coder('Malaizyo', 'Rap', 20, 'JavaScript')
+
+console.log(Malaizyo.getAge())
+// console.log(Malaizyo.lang)
+// console.log(Malaizyo.age)
+
+class WebDev extends Coder {
+  constructor(
+    public computer: string,
+    readonly name: string,
+    music: string,
+    age: number
+  ) {
+    super(name, music, age)
+    this.computer = computer
+  }
+
+  public getLang() {
+    return `I'm a ${this.lang} developer`
+  }
+}
+
+const Sara = new WebDev('MacBook', 'Sara', 'Pop', 20)
+console.log(Sara.getLang())
+
+class Peeps {
+  static count: number = 0
+
+  static getCount(): number {
+    return Peeps.count
+  }
+
+  public id: number
+
+  constructor(public name: string) {
+    this.id = ++Peeps.count
+  }
+}
+
+const Zodo = new Peeps('Zodo')
+const Zozo = new Peeps('Zozo')
+const Koko = new Peeps('Koko')
+
+console.log(Peeps.getCount())
+
+class Bands {
+  private dataState: string[]
+
+  constructor() {
+    this.dataState = []
+  }
+
+  public get data(): string[] {
+    return this.dataState
+  }
+}
