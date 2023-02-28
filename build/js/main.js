338aaf595e112a3f0700b27903c6bc7a5040ff86
Zodo <malaizyogmuzumala@gmail.com>
2023-02-28 20:03:18 +0200

    still here
    Working and more work

--- build/js/main.js
+++ build/js/main.js
@@ -1,1 +1,40 @@
 "use strict";
+const updateAssignment = (assign, propsToUpdate) => {
+    return Object.assign(Object.assign({}, assign), propsToUpdate);
+};
+const assign1 = {
+    studentId: '123',
+    title: 'Assignment 1',
+    grade: 100,
+};
+console.log(updateAssignment(assign1, { grade: 90 }));
+const assignGraded = updateAssignment(assign1, { grade: 90 });
+const recordAssignment = (assign) => {
+    //do something
+    return assign;
+};
+//required and readonly
+const assignVerified = Object.assign(Object.assign({}, assign1), { verified: true });
+// assignVerified.grade = 90
+recordAssignment(Object.assign(Object.assign({}, assign1), { verified: true }));
+//record
+const hexColorMap = {
+    red: '#ff0000',
+    green: '#00ff00',
+    blue: '#0000ff',
+};
+const finalGrades = {
+    Sara: 'A',
+    Kelly: 'U',
+};
+const gradeData = {
+    Sara: {
+        assignment1: 100,
+        assignment2: 100,
+    },
+    Kelly: { assignment1: 90, assignment2: 80 },
+};
+const score = {
+    studentId: '123',
+    grade: 100,
+};
---
+++ src/main.ts
@@ -0,0 +1,79 @@
+interface Assignment {
+  studentId: string
+  title: string
+  grade: number
+  verified?: boolean
+}
+
+const updateAssignment = (
+  assign: Assignment,
+  propsToUpdate: Partial<Assignment>
+): Assignment => {
+  return {
+    ...assign,
+    ...propsToUpdate,
+  }
+}
+
+const assign1: Assignment = {
+  studentId: '123',
+  title: 'Assignment 1',
+  grade: 100,
+}
+
+console.log(updateAssignment(assign1, { grade: 90 }))
+
+const assignGraded: Assignment = updateAssignment(assign1, { grade: 90 })
+
+const recordAssignment = (assign: Required<Assignment>) => {
+  //do something
+  return assign
+}
+
+//required and readonly
+
+const assignVerified: Readonly<Assignment> = {
+  ...assign1,
+  verified: true,
+}
+
+// assignVerified.grade = 90
+recordAssignment({ ...assign1, verified: true })
+
+//record
+const hexColorMap: Record<string, string> = {
+  red: '#ff0000',
+  green: '#00ff00',
+  blue: '#0000ff',
+}
+
+type Students = 'Sara' | 'Kelly'
+type LetterGrades = 'A' | 'B' | 'C' | 'D' | 'U'
+
+const finalGrades: Record<Students, LetterGrades> = {
+  Sara: 'A',
+  Kelly: 'U',
+}
+
+interface Grades {
+  assignment1: number
+  assignment2: number
+}
+const gradeData: Record<Students, Grades> = {
+  Sara: {
+    assignment1: 100,
+    assignment2: 100,
+  },
+  Kelly: { assignment1: 90, assignment2: 80 },
+}
+
+//Pick
+
+type AssignResult = Pick<Assignment, 'studentId' | 'grade'>
+
+const score: AssignResult = {
+  studentId: '123',
+  grade: 100,
+}
+
+type AssignPreview = Omit<Assignment, 'verified' | 'grade'>
